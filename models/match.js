033c1fca20cfb7a5debf4768ac2f088240ddfd03
Christoph Szymanski <spamerella@gmx.de>
2018-03-25 19:21:24 +0200

    Unified two apps into one

---
+++ models/match.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+
+var matchSchema = mongoose.Schema({
+    "match" : [{
+        "team": [String],
+        "score": Number 
+    }]
+});
+
+module.exports = mongoose.model('Match', matchSchema);
