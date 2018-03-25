cbe440bcc23f98342a80e9f822c842c7da159ca8
Christoph Szymanski <christoph.szymanski@sap.com>
2018-03-25 16:09:26 +0200

    unified app

--- tests/supertest.js
+++ tests/supertest.js
@@ -1,22 +1,62 @@
-const request = require('supertest');
-const app = require('./../app.js');
+var request = require('supertest');
+var app = require('../app.js');
+var mongoose = require('mongoose');
 
-describe('Using supertest for testing the API', function() {
-    it('POST without body.text returns 400', function() {
-        let message =  "something=@Laura and @Dennis crushed @Peter + @Mary today, winning 10:0";
-        request(app)
-            .post('/')
-            .set('Content-Type', 'application/x-www-form-urlencoded')
-            .send(message)
-            .expect(400);
+describe('Supertesting the API', function() {
+    describe('Testing the / API (the reconfubulator)', function() {
+        it('Missing body.text returns 400', function() {
+            let message =  "something=@Laura and @Dennis crushed @Peter + @Mary today, winning 10:0";
+            request(app)
+                .post('/')
+                .set('Content-Type', 'application/x-www-form-urlencoded')
+                .send(message)
+                .expect(400);
+        });
+
+        it('POST with body.text', function() {
+            let message = "text=@Laura and @Dennis crushed @Peter + @Mary today, winning 10:0";
+            request(app)
+                .post('/')
+                .set('Content-Type', 'application/x-www-form-urlencoded')
+                .send(message)
+                .expect(200, "@Laura and @Dennis played 10:0 against @Peter and @Mary.");
+        });
     });
 
-    it('POST with body.text', function() {
-        let message = "text=@Laura and @Dennis crushed @Peter + @Mary today, winning 10:0";
-        request(app)
-            .post('/')
-            .set('Content-Type', 'application/x-www-form-urlencoded')
-            .send(message)
-            .expect(200, "@Laura and @Dennis played 10:0 against @Peter and @Mary.");
-    });
+
+    describe('Testing /results API (storage of results in mongodb)', function() {
+        it('Create, read and delete a resource', function(done) {
+            request(app)
+                .get('/results')
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .then(function post() {
+    
+                    var match = 
+                        { "match": [{ 
+                            "team": ["Alejandra", "Sergey"], 
+                            "score": 10
+                        }, {
+                            "team": ["Marcus", "Ken"], 
+                            "score": 7
+                        }]};
+    
+                    return request(app)
+                        .post('/results')
+                        .send(match)
+                        .set('Content-Type', 'application/json')
+                        .expect(201)
+                        .expect('Location', /\/5.*/);
+                 }).then(function get() {
+                     request(app)
+                        .get('/results')
+                        .set('Accept', 'application/json')
+                        .expect(200)
+                       // .expect('match')
+                        .end(done);
+                 });
+        });
+    })
 });
+
---
+++ tests/test.js
@@ -0,0 +1,64 @@
+var validate = require("./../validate.js")
+var assert = require('assert');
+
+describe('Test result score validation:', function() {
+    it('10:9 is a valid result', function() {
+        var match = 
+            { "match": [{ 
+                "team": ["Alejandra", "Sergey"], 
+                "score": 10
+            }, {
+                "team": ["Marcus", "Ken"], 
+                "score": 9
+            }]};
+        assert.equal(validate(match), true);
+    });
+    
+    it('no draw is allowed', function() {
+        var match = 
+            { "match": [{ 
+                "team": ["Alejandra", "Sergey"], 
+                "score": 7
+            }, {
+                "team": ["Marcus", "Ken"], 
+                "score": 7
+            }]};
+        assert.equal(validate(match), false);
+    });
+
+    it('only one winner', function() {
+        var match = 
+            { "match": [{ 
+                "team": ["Alejandra", "Sergey"], 
+                "score": 10
+            }, {
+                "team": ["Marcus", "Ken"], 
+                "score": 10
+            }]};
+        assert.equal(validate(match), false);
+    });
+
+    it('score cant be higher than 10', function() {
+        var match = 
+            { "match": [{ 
+                "team": ["Alejandra", "Sergey"], 
+                "score": 11
+            }, {
+                "team": ["Marcus", "Ken"], 
+                "score": 10
+            }]};
+        assert.equal(validate(match), false);
+    });
+
+    it('Score cant be lower than 0', function() {
+        var match = 
+            { "match": [{ 
+                "team": ["Alejandra", "Sergey"], 
+                "score": -1
+            }, {
+                "team": ["Marcus", "Ken"], 
+                "score": 10
+            }]};
+        assert.equal(validate(match), false);
+    });
+});
