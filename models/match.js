848aaaba68f616d0e1af276702f792a9326079f0
Christoph Szymanski <christoph.szymanski@sap.com>
2018-03-26 11:18:10 +0200

    linting + initial --fix

--- models/match.js
+++ models/match.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose');
 
 var matchSchema = mongoose.Schema({
-    "match" : [{
-        "team": [String],
-        "score": Number 
-    }]
+  'match' : [{
+    'team': [String],
+    'score': Number 
+  }]
 });
 
 module.exports = mongoose.model('Match', matchSchema);
--- tests/test.js
+++ tests/test.js
@@ -1,64 +1,64 @@
-var validate = require("./../validate.js")
+var validate = require('./../validate.js');
 var assert = require('assert');
 
 describe('Test result score validation:', function() {
-    it('10:9 is a valid result', function() {
-        var match = 
-            { "match": [{ 
-                "team": ["Alejandra", "Sergey"], 
-                "score": 10
+  it('10:9 is a valid result', function() {
+    var match = 
+            { 'match': [{ 
+              'team': ['Alejandra', 'Sergey'], 
+              'score': 10
             }, {
-                "team": ["Marcus", "Ken"], 
-                "score": 9
+              'team': ['Marcus', 'Ken'], 
+              'score': 9
             }]};
-        assert.equal(validate(match), true);
-    });
+    assert.equal(validate(match), true);
+  });
     
-    it('no draw is allowed', function() {
-        var match = 
-            { "match": [{ 
-                "team": ["Alejandra", "Sergey"], 
-                "score": 7
+  it('no draw is allowed', function() {
+    var match = 
+            { 'match': [{ 
+              'team': ['Alejandra', 'Sergey'], 
+              'score': 7
             }, {
-                "team": ["Marcus", "Ken"], 
-                "score": 7
+              'team': ['Marcus', 'Ken'], 
+              'score': 7
             }]};
-        assert.equal(validate(match), false);
-    });
+    assert.equal(validate(match), false);
+  });
 
-    it('only one winner', function() {
-        var match = 
-            { "match": [{ 
-                "team": ["Alejandra", "Sergey"], 
-                "score": 10
+  it('only one winner', function() {
+    var match = 
+            { 'match': [{ 
+              'team': ['Alejandra', 'Sergey'], 
+              'score': 10
             }, {
-                "team": ["Marcus", "Ken"], 
-                "score": 10
+              'team': ['Marcus', 'Ken'], 
+              'score': 10
             }]};
-        assert.equal(validate(match), false);
-    });
+    assert.equal(validate(match), false);
+  });
 
-    it('score cant be higher than 10', function() {
-        var match = 
-            { "match": [{ 
-                "team": ["Alejandra", "Sergey"], 
-                "score": 11
+  it('score cant be higher than 10', function() {
+    var match = 
+            { 'match': [{ 
+              'team': ['Alejandra', 'Sergey'], 
+              'score': 11
             }, {
-                "team": ["Marcus", "Ken"], 
-                "score": 10
+              'team': ['Marcus', 'Ken'], 
+              'score': 10
             }]};
-        assert.equal(validate(match), false);
-    });
+    assert.equal(validate(match), false);
+  });
 
-    it('Score cant be lower than 0', function() {
-        var match = 
-            { "match": [{ 
-                "team": ["Alejandra", "Sergey"], 
-                "score": -1
+  it('Score cant be lower than 0', function() {
+    var match = 
+            { 'match': [{ 
+              'team': ['Alejandra', 'Sergey'], 
+              'score': -1
             }, {
-                "team": ["Marcus", "Ken"], 
-                "score": 10
+              'team': ['Marcus', 'Ken'], 
+              'score': 10
             }]};
-        assert.equal(validate(match), false);
-    });
+    assert.equal(validate(match), false);
+  });
 });
