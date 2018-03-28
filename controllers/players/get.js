c4586f8809467662a6b34f7e2c62762da32d83c9
Christoph Szymanski <christoph.szymanski@sap.com>
2018-03-28 18:58:29 +0200

    GET /player/:name mongoose query

--- controllers/players/get.js
+++ controllers/players/get.js
@@ -1,13 +1,15 @@
 const MatchModel = require('./../../models/match.js');
 
 module.exports = function(request, response) {
-    MatchModel.find(
-      { "team": request.params.name },
-      function (err, docs) {
-        if (err) {
-          response.status(404).send(err);
-          return;
-        }
-        response.status(200).send(docs);
-      });
-  }
+  let query = { "match.team":  request.params.name };
+
+  MatchModel.find(
+    query,
+    function (err, docs) {
+      if (err) {
+        response.status(404).send(err);
+        return;
+      }
+      response.status(200).send(docs);
+  });
+};
