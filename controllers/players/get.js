e8b041e99f56e5f07681e42b6bf7a1b3ffb75238
Christoph Szymanski <christoph.szymanski@sap.com>
2018-04-02 16:29:03 +0200

    storage: result creation time + tests

--- controllers/players/get.js
+++ controllers/players/get.js
@@ -2,13 +2,13 @@ const MatchModel = require('./../../models/match.js');
 
 module.exports = function(request, response) {
   let query = { 'match.team':  request.params.name };
-  let limit = request.query.limit || 20;
-  let sort = request.query.sort || '-_id'
+  let limit = parseInt(request.query.limit) || 20;
+  let sort = request.query.sort || '-_id';
   
   MatchModel
     .find(query)
-    .limit(10)
-    .sort({ '_id': -1 })
+    .limit(limit)
+    .sort(sort)
     .exec(function (err, docs) {
       if (err) {
         response.status(404).send(err);
--- tests/models/match.js
+++ tests/models/match.js
@@ -1,157 +1,210 @@
 const MatchModel = require('../../models/match');
-const assert = require('assert');
+const assert = require('chai').assert;
+require('../../database');
 
-describe('Test result score validation:', function() {
-  it('10:9 is a valid result', function() {
-    let match = { 'match': [{ 
-      'team': ['Alejandra', 'Sergey'], 
-      'score': 9
-    }, {
-      'team': ['Marcus', 'Ken'], 
-      'score': 10
-    }]};
-
-    let error = new MatchModel(match).validateSync();
-    assert.equal(error, null);
-  });
-
-  it('no draw is allowed', function() {
-    let match = 
-      { 'match': [{ 
+describe('Match result model tests', function() {
+  describe('Creation time:', function() {
+    it('Automated creation of valid date', function(done) {
+      let match = { 'match': [{ 
         'team': ['Alejandra', 'Sergey'], 
-        'score': 7
+        'score': 9
       }, {
         'team': ['Marcus', 'Ken'], 
-        'score': 7
+        'score': 10
       }]};
-    let error = new MatchModel(match).validateSync();
-    assert.notEqual(error, null);
+      
+      let model = new MatchModel(match);
+      model
+        .save(function(err, row) {
+          if (err) {
+            assert.fail(err.message);
+            done();
+          } else {
+            assert.notEqual(row.created, undefined);
+            assert.equal(row.created.toString().substr(0, 16), new Date().toString().substr(0, 16));
+          }
+          done();
+        });
+    });
+
+    it('If created is give, we won\'t mess with it', function(done) {
+      let match = { 
+        'created': 'Tue, 01 Jan 2008 22:00:00 GMT',
+        'match': [{ 
+          'team': ['Alejandra', 'Sergey'], 
+          'score': 9
+        }, {
+          'team': ['Marcus', 'Ken'], 
+          'score': 10
+        }]};
+        
+      let model = new MatchModel(match);
+      model
+        .save(function(err, row) {
+          if (err) {
+            assert.fail(err);
+          } else {
+            assert.equal(row.created.toString(), new Date('2008-01-01T22:00:00.000Z'));
+          }
+          done();
+        });
+    });
   });
 
-  it('only one winner', function() {
-    let match = 
-            { 'match': [{ 
-              'team': ['Alejandra', 'Sergey'], 
-              'score': 10
-            }, {
-              'team': ['Marcus', 'Ken'], 
-              'score': 10
-            }]};
-    let error = new MatchModel(match).validateSync();
-    assert.notEqual(error, null);
-  });
+  
 
-  it('score can\'t be higher than 10', function() {
-    let match = { 'match': [{ 
-      'team': ['Alejandra', 'Sergey'], 
-      'score': 11
-    }, {
-      'team': ['Marcus', 'Ken'], 
-      'score': 10
-    }]};
-    let error = new MatchModel(match).validateSync();
-    assert.notEqual(error, null);
-  });
-
-  it('Score can\'t be lower than 0', function() {
-    let match = 
-      { 'match': [{ 
+  describe('Pre save validation:', function() {
+    it('10:9 is a valid result', function() {
+      let match = { 'match': [{ 
         'team': ['Alejandra', 'Sergey'], 
-        'score': -1
+        'score': 9
       }, {
         'team': ['Marcus', 'Ken'], 
         'score': 10
       }]};
-    let error = new MatchModel(match).validateSync();
-    assert.notEqual(error, null);
-  });
 
-  it('Score can\'t be lower than 0', function() {
-    let match = 
-      { 'match': [{ 
+      let error = new MatchModel(match).validateSync();
+      assert.equal(error, null);
+    });
+
+    it('no draw is allowed', function() {
+      let match = 
+        { 'match': [{ 
+          'team': ['Alejandra', 'Sergey'], 
+          'score': 7
+        }, {
+          'team': ['Marcus', 'Ken'], 
+          'score': 7
+        }]};
+      let error = new MatchModel(match).validateSync();
+      assert.notEqual(error, null);
+    });
+
+    it('only one winner', function() {
+      let match = 
+              { 'match': [{ 
+                'team': ['Alejandra', 'Sergey'], 
+                'score': 10
+              }, {
+                'team': ['Marcus', 'Ken'], 
+                'score': 10
+              }]};
+      let error = new MatchModel(match).validateSync();
+      assert.notEqual(error, null);
+    });
+
+    it('score can\'t be higher than 10', function() {
+      let match = { 'match': [{ 
         'team': ['Alejandra', 'Sergey'], 
-        'score': -1
+        'score': 11
       }, {
         'team': ['Marcus', 'Ken'], 
         'score': 10
       }]};
-    let error = new MatchModel(match).validateSync();
-    assert.notEqual(error, null);
-  });
+      let error = new MatchModel(match).validateSync();
+      assert.notEqual(error, null);
+    });
 
-  it('Score must be a number', function() {
-    let match = 
-      { 'match': [{ 
-        'team': ['Alejandra', 'Sergey'], 
-        'score': 'one'
-      }, {
-        'team': ['Marcus', 'Ken'], 
-        'score': 10
-      }]};
-    let error = new MatchModel(match).validateSync();
-    assert.notEqual(error, null);
-  });
+    it('Score can\'t be lower than 0', function() {
+      let match = 
+        { 'match': [{ 
+          'team': ['Alejandra', 'Sergey'], 
+          'score': -1
+        }, {
+          'team': ['Marcus', 'Ken'], 
+          'score': 10
+        }]};
+      let error = new MatchModel(match).validateSync();
+      assert.notEqual(error, null);
+    });
 
-  it('Score must be set', function() {
-    let match = 
-      { 'match': [{ 
-        'team': ['Alejandra', 'Sergey']
-      }, {
-        'team': ['Marcus', 'Ken'], 
-        'score': 10
-      }]};
-    let error = new MatchModel(match).validateSync();
-    assert.notEqual(error, null);
-  });
+    it('Score can\'t be lower than 0', function() {
+      let match = 
+        { 'match': [{ 
+          'team': ['Alejandra', 'Sergey'], 
+          'score': -1
+        }, {
+          'team': ['Marcus', 'Ken'], 
+          'score': 10
+        }]};
+      let error = new MatchModel(match).validateSync();
+      assert.notEqual(error, null);
+    });
 
-  it('Team size of 1 is possible', function() {
-    let match = 
-      { 'match': [{ 
-        'team': ['Roman'],
-        'score': 9
-      }, {
-        'team': ['Marcus', 'Ken'], 
-        'score': 10
-      }]};
-    let error = new MatchModel(match).validateSync();
-    assert.equal(error, null);
-  });
+    it('Score must be a number', function() {
+      let match = 
+        { 'match': [{ 
+          'team': ['Alejandra', 'Sergey'], 
+          'score': 'one'
+        }, {
+          'team': ['Marcus', 'Ken'], 
+          'score': 10
+        }]};
+      let error = new MatchModel(match).validateSync();
+      assert.notEqual(error, null);
+    });
 
-  it('Team size of 3 is not possible', function() {
-    let match = 
-      { 'match': [{ 
-        'team': ['Roman', 'Kay', 'Thorsten'],
-        'score': 9
-      }, {
-        'team': ['Marcus', 'Ken'], 
-        'score': 10
-      }]};
-    let error = new MatchModel(match).validateSync();
-    assert.notEqual(error, null);
-  });
+    it('Score must be set', function() {
+      let match = 
+        { 'match': [{ 
+          'team': ['Alejandra', 'Sergey']
+        }, {
+          'team': ['Marcus', 'Ken'], 
+          'score': 10
+        }]};
+      let error = new MatchModel(match).validateSync();
+      assert.notEqual(error, null);
+    });
 
-  it('team must be set', function() {
-    let match = 
-      { 'match': [{ 
-        'score': 9
-      }, {
-        'team': ['Marcus', 'Ken'], 
-        'score': 10
-      }]};
-    let error = new MatchModel(match).validateSync();
-    assert.notEqual(error, null);
-  });
+    it('Team size of 1 is possible', function() {
+      let match = 
+        { 'match': [{ 
+          'team': ['Roman'],
+          'score': 9
+        }, {
+          'team': ['Marcus', 'Ken'], 
+          'score': 10
+        }]};
+      let error = new MatchModel(match).validateSync();
+      assert.equal(error, null);
+    });
 
-  it('there must be at least one team with 10 goals', function() {
-    let match = 
-      { 'match': [{ 
-        'team': ['Parsa', 'Holger'], 
-        'score': 9
-      }, {
-        'team': ['Marcus', 'Ken'], 
-        'score': 9
-      }]};
-    let error = new MatchModel(match).validateSync();
-    assert.notEqual(error, null);
-  }); 
+    it('Team size of 3 is not possible', function() {
+      let match = 
+        { 'match': [{ 
+          'team': ['Roman', 'Kay', 'Thorsten'],
+          'score': 9
+        }, {
+          'team': ['Marcus', 'Ken'], 
+          'score': 10
+        }]};
+      let error = new MatchModel(match).validateSync();
+      assert.notEqual(error, null);
+    });
+
+    it('team must be set', function() {
+      let match = 
+        { 'match': [{ 
+          'score': 9
+        }, {
+          'team': ['Marcus', 'Ken'], 
+          'score': 10
+        }]};
+      let error = new MatchModel(match).validateSync();
+      assert.notEqual(error, null);
+    });
+
+    it('there must be at least one team with 10 goals', function() {
+      let match = 
+        { 'match': [{ 
+          'team': ['Parsa', 'Holger'], 
+          'score': 9
+        }, {
+          'team': ['Marcus', 'Ken'], 
+          'score': 9
+        }]};
+      let error = new MatchModel(match).validateSync();
+      assert.notEqual(error, null);
+    }); 
+  });
 });
